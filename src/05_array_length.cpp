62316800a35af0e7b9dc9c6064dfec45fd55d967
fotcorn <fotcorn@gmail.com>
2020-02-25 16:42:07 +0000

    array_length

---
+++ src/05_array_length.cpp
@@ -0,0 +1,20 @@
+#include <cstddef>
+#include <iostream>
+
+template <typename V, std::size_t L> constexpr std::size_t array_length(const V (&array)[L]) {
+    return L;
+}
+
+int my_random() {
+    return 5;
+}
+
+int main() {
+    int array[] = {1, 2, 3, 4};
+    std::cout << array_length(array) << std::endl;
+
+    // we can use array_length for compile time constants because its defined as constexpr
+    int other_array[array_length(array)];
+    std::cout << array_length(other_array) << std::endl;
+    return 0;
+}
